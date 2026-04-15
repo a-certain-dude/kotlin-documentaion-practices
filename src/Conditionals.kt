ae03cbc467a865f8fc937732749418e5e8623c1e
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-04-15 18:49:14 +0000

    if conditional

--- src/Conditionals.kt
+++ src/Conditionals.kt
@@ -1,3 +1,19 @@
-class Conditionals {
+import kotlin.random.Random
+
+object Conditionals {
+    /* Create a simple game where
+     you win if throwing two dice
+     results in the same number.
+     Use if to print You win :) if the dice match, or You lose :( otherwise.*/
+
+    fun ifCondition() {
+        val firstResult = Random.nextInt(until = 6)
+        val secondResult = Random.nextInt(until = 6)
+
+        if (firstResult == secondResult)
+            println("You win :)")
+        else
+            println("You loose :(")
+    }
 
 }
