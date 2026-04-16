76577af3eb55bd36b63a99603219d0ce8f24676f
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-04-16 14:26:08 +0000

    function |singleExpression

--- src/Functions.kt
+++ src/Functions.kt
@@ -7,7 +7,13 @@ object Functions {
     of a circle in integer format as a parameter and outputs the area of that circle.*/
 
     fun circleArea(radius: Int): Double {
-       return PI * radius * radius
+        return PI * radius * radius
     }
+
+    fun circleAreaSingleExpression(radius: Int): Double = PI * radius * radius
+    Exercise 3
+    You have a function that translates a time interval given in hours, minutes, and seconds into seconds. In most cases, you need to pass only one or two function
+    parameters while the rest are equal to 0. Improve the function and the code that calls it by using default parameter values and named arguments so that the code is
+    easier to read
 
 }
