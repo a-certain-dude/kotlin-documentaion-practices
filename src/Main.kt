cd289348327983baa7228a3101116dbbca2d3f25
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-01 13:20:24 +0000

    Scope functions|with

--- src/Main.kt
+++ src/Main.kt
@@ -39,5 +39,38 @@ fun main() {
     println()
     println("JOSEPH".toLowerCaseString())
 
+    println()
+    println("apply")
+    val canvas: Canvas = Canvas().apply {
+        rect(x = 10, y = 10, w = 5, h = 8)
+        circ(x = 10, y = 10, r = 5)
+        text(x = 10, y = 10, str = "Kwame")
+    }
+
+    println()
+
+    println("mainMonitor")
+    val mainMonitorPrimaryBufferBackedCanvas = Canvas()
+    mainMonitorPrimaryBufferBackedCanvas.text(10, 10, "Foo")
+    mainMonitorPrimaryBufferBackedCanvas.rect(20, 30, 100, 50)
+    mainMonitorPrimaryBufferBackedCanvas.circ(40, 60, 25)
+    mainMonitorPrimaryBufferBackedCanvas.text(15, 45, "Hello")
+    mainMonitorPrimaryBufferBackedCanvas.rect(70, 80, 150, 100)
+    mainMonitorPrimaryBufferBackedCanvas.circ(90, 110, 40)
+    mainMonitorPrimaryBufferBackedCanvas.text(35, 55, "World")
+    mainMonitorPrimaryBufferBackedCanvas.rect(120, 140, 200, 75)
+    mainMonitorPrimaryBufferBackedCanvas.circ(160, 180, 55)
+    mainMonitorPrimaryBufferBackedCanvas.text(50, 70, "Kotlin")
+
+    println()
+    println("secondary")
+    val secondaryBufferBackedCanvas /*receiver object*/  = Canvas()
+    with(secondaryBufferBackedCanvas){
+        rect(x = 10, y = 10, w = 5, h = 8)
+        circ(x = 10, y = 10, r = 5)
+        text(x = 10, y = 10, str = "Kwame")
+        rect(10,4,7,8)
+        println("receiver : $this")
+    }
 
 }
