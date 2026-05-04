d86812edcb9a3796bd7210c4dfb4b8a60ee8115c
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-04 16:25:00 +0000

    document code

--- src/lambdas/CopyOfListOfIntegers.kt
+++ src/lambdas/CopyOfListOfIntegers.kt
@@ -15,12 +15,35 @@ fun List<Int>.incremented(): List<Int> {
     }
 }
 
+
+/*Correct answer for the Documentation*/
+fun List<Int>.incrementX(): List<Int> {
+    val originalList = this // this(which is the receiver) is List<Int>
+    return buildList {
+        for (n in originalList) {
+            add(n + 1)
+        }
+    }
+}
+
 fun main() {
+    /*This function contains
+     the various solution of the extended
+     incremented/incrementX function*/
+
+
     val originalList = listOf(1, 2, 3)
     originalList.incremented()
     /*val newList = originalList.incremented()
-    newList  is not needed as original.incremented naturally prints
+    newList  is not needed as original.incremented naturally prints the values
     println(newList)
     prints [] which is not needed
     */
+    println("****************************")
+
+    val anotherOriginalList = listOf(4, 5, 6)
+    val newListMadeOutOfAnotherOriginalList = anotherOriginalList.incrementX()
+    println(newListMadeOutOfAnotherOriginalList)
+
+
 }
