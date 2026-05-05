0e4a29732b3b7194ebb14fc6f7b15712091f49b3
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-05 20:29:35 +0000

    Classes and Interfaces|SmartHome

--- src/classesAndInterfaces/smartHome.kt
+++ src/classesAndInterfaces/smartHome.kt
@@ -1,2 +1,19 @@
 package classesAndInterfaces
 
+
+class SmartLight(name: String) :
+    SmartDevice(name = name) {
+
+    override fun turnOn() {
+        println("$name is now ON")
+    }
+
+    override fun turnOff() {
+        println("$name is now OFF")
+    }
+
+    fun adjustBrightness(level: Int) {
+        println("Brightness Level of $name is : $level%")
+    }
+
+}
