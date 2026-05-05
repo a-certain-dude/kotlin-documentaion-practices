52df9eb42426d8cbb0ea8d977c5093902d1e5d9b
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-05 20:29:43 +0000

    Classes

--- src/classesAndInterfaces/smartHome.kt
+++ src/classesAndInterfaces/smartHome.kt
@@ -1,5 +1,29 @@
 package classesAndInterfaces
 
+/*
+Exercise 1
+Imagine you're working on a smart home system. A smart home typically has different types
+of devices that all have some basic features but also unique behaviors.
+In the code sample below, complete the abstract class called SmartDevice so that the child
+class SmartLight can compile successfully.
+
+Then, create another child class called SmartThermostat that inherits from the
+SmartDevice class and implements turnOn() and turnOff() functions that return print
+statements describing which thermostat is heating or turned off.
+Finally, add another function called adjustTemperature() that accepts a temperature
+measurement as an input and prints: $name thermostat set to $temperature°C.
+Hint
+In the SmartDevice class, add the turnOn() and turnOff() functions so that you can override their behavior later in the SmartThermostat class.
+*/
+
+
+
+
+abstract class SmartDevice(val name: String) {
+    abstract fun turnOn()
+    abstract fun turnOff()
+
+}
 
 class SmartLight(name: String) :
     SmartDevice(name = name) {
