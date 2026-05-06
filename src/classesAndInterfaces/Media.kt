be3965d7cbf4a70543ead5f92481b3c1944c758b
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-06 16:32:48 +0000

    Classes and Interfaces|Media

---
+++ src/classesAndInterfaces/Media.kt
@@ -0,0 +1,10 @@
+package classesAndInterfaces
+
+Exercise 2
+Create an interface called Media that you can use to implement specific media classes like Audio, Video, or Podcast. Your interface must include:
+A property called title to represent the title of the media.
+A function called play() to play the media.
+Then, create a class called Audio that implements the Media interface. The Audio class must use the title property in its constructor as well as have an additional
+property called composer that has String type. In the class, implement the play() function to print the following: "Playing audio: $title, composed by $composer".
+Hint
+You can use the override keyword in class headers to implement a property from an interface in the constructor.
