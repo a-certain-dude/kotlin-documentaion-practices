5f8c0c93e5d10b4e88cf11f9aa113f654c4b6f46
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-07 21:21:08 +0000

    Classes and Interfaces| interface, abstract

--- src/classesAndInterfaces/PaymentProcessing.kt
+++ src/classesAndInterfaces/PaymentProcessing.kt
@@ -1,13 +1,48 @@
 package classesAndInterfaces
 
+/*
 Exercise 3
-You're building a payment processing system for an e-commerce application. Each payment method needs to be able to authorize a payment and process a
+You're building a payment processing system for an e-commerce application.
+Each payment method needs to be able to authorize a payment and process a
 transaction. Some payments also need to be able to process refunds.
 1. In the Refundable interface, add a function called refund() to process refunds.
 2. In the PaymentMethod abstract class:
-    Add a function called authorize() that takes an amount and prints a message containing the amount.
+    Add a function called authorize() that takes an amount and prints a message
+     containing the amount.
 Add an abstract function called processPayment() that also takes an amount.
-3. Create a class called CreditCard that implements the Refundable interface and PaymentMethod abstract class. In this class, add implementations for the refund()
+3. Create a class called CreditCard that implements the Refundable
+interface and PaymentMethod abstract class.
+In this class, add implementations for the refund()
 and processPayment() functions so that they print the following statements:
 "Refunding $amount to the credit card."
-"Processing credit card payment of $amount."
+"Processing credit card payment of $amount."*/
+
+interface Refundable {
+    fun refund()
+}
+
+abstract class PaymentMethod() {
+    fun authorize(amount: Double) {
+        println("Amount of $amount has been authorized")
+    }
+    abstract fun processPayment(amount: Double)
+}
+
+class CreditCard() : Refundable, PaymentMethod() {
+    val amount = 100.00
+    override fun refund() {
+        println("Refunding $amount to the CreditCard")
+    }
+
+    override fun processPayment(amount: Double) {
+        println("Processing CreditCard of $amount")
+    }
+}
+
+fun main() {
+    CreditCard().apply {
+        authorize(this.amount)
+        processPayment(this.amount)
+        refund()
+    }
+}
