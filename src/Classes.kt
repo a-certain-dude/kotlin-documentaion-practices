057ddf7c4c1691220151d2a51d20cef9cf5b2d94
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-07 22:27:53 +0000

    add main function

--- src/Classes.kt
+++ src/Classes.kt
@@ -86,3 +86,15 @@ class RandomEmployeeGenerator(var minSalary: Int, var maxSalary: Int) {
     }
 
 }
+
+fun main() {
+    val employee = Classes.Employee("Osei", 35)
+    employee.salary += 10
+    println(employee.name + " " + employee.salary)
+    println()
+    Classes.main()
+    val rand = RandomEmployeeGenerator(10, 30)
+    println(rand.generateEmployee())
+    println(rand.minSalary)
+    println(rand.maxSalary)
+}
