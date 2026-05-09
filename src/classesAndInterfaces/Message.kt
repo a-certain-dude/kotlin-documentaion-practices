93c23b184826d310e2446f127f7ea7a9c2862ed7
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-09 17:46:37 +0000

    messaging,smart message

--- src/classesAndInterfaces/Message.kt
+++ src/classesAndInterfaces/Message.kt
@@ -1,2 +1,35 @@
 package classesAndInterfaces
+/*
+Exercise 4
+You have a simple messaging app that has some basic functionality,
+but you want to add some functionality for smart messages without
+significantly duplicating your code.
+
+In the code below, define a class called SmartMessenger that inherits from the
+Messenger interface but delegates the implementation to an instance of the
+BasicMessenger class.
+
+In the SmartMessenger class, override the sendMessage() function to send smart messages.
+The function must accept a message as an input and return a printed
+statement: "Sending a smart message: $message".
+
+In addition, call the sendMessage() function from the BasicMessenger class and prefix
+the message with [smart].
+You don't need to rewrite the receiveMessage() function in the SmartMessenger class.*/
+
+
+interface Messaging {
+    fun sendMessage(message: String)
+    fun receiveMessage(): String
+}
+
+class SmartMessenger : Messaging {
+    override fun sendMessage(message: String) {
+        println("Sending message: $message ")
+    }
+    override fun receiveMessage(): String {
+        return "Message received"
+    }
+}
+
 
