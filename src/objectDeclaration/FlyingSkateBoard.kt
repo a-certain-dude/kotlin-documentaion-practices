e053e11ddfa25877c46b3004dc448dfc0dd1f7cb
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-10 11:12:51 +0000

    flying skateboard

---
+++ src/objectDeclaration/FlyingSkateBoard.kt
@@ -0,0 +1,28 @@
+package objectDeclaration
+
+/*
+Exercise 2
+Create an object declaration that inherits from the Vehicle interface to create a unique
+vehicle type: FlyingSkateboard. Implement the name property and the move()
+function in your object so that the following code in the main() function runs
+successfully:*/
+
+interface Vehicle {
+    val name: String
+    fun move(): String
+}
+
+object FlyingSkateBoard : Vehicle {
+    override val name: String = "FlyingSkate"
+    override fun move(): String {
+        return "A Flying Skate Board is moving"
+    }
+
+    fun flyingSound() = println("Woohoooooo hahaaa ")
+}
+
+fun main() {
+    println("Name of vehicle:  ${FlyingSkateBoard.name}")
+    println("Name of vehicle:  ${FlyingSkateBoard.move()}")
+    FlyingSkateBoard.flyingSound()
+}
