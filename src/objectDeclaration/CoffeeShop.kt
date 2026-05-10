941e8308d959bba1769a61fc14750e645256c2dc
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-10 10:49:16 +0000

    order one data object

--- src/objectDeclaration/CoffeeShop.kt
+++ src/objectDeclaration/CoffeeShop.kt
@@ -17,3 +17,45 @@ data object OrderOne : Order {
     override val customerName: String = "Joseph"
     override val orderTotal: Double = 34.1
 }
+
+data object OrderTwo : Order {
+    override val orderId: String = "235"
+    override val customerName: String = "Joan"
+    override val orderTotal: Double = 23.4
+}
+
+data object CoffeeShop {
+    val order = OrderOne.apply {
+        println(orderId)
+        println(customerName)
+        println(orderTotal)
+    }
+
+    val anotherOrder = OrderTwo.apply {
+        println(orderId)
+        println(customerName)
+        println(orderTotal)
+    }
+}
+
+fun main() {
+    println("Order name: $OrderOne")
+    println("Order name: $OrderTwo")
+
+    if (OrderOne.equals(OrderTwo)) {
+        println("The orders are identical.")
+    } else {
+        println("The orders are unique.")
+        // The orders are unique.
+    }
+    if (OrderOne.equals(OrderTwo)) {
+        println("The orders are identical.")
+    } else {
+        println("The orders are unique.")
+        // The orders are unique.
+    }
+
+    CoffeeShop.order
+    CoffeeShop.anotherOrder
+
+}
