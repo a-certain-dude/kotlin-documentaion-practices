5cf76dadcbe52b604f4fb19cf18af1500b676aec
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-10 07:40:02 +0000

    Object declaration|coffee shop

---
+++ src/objectDeclaration/CoffeeShop.kt
@@ -0,0 +1,8 @@
+package objectDeclaration
+
+/*
+Exercise 1
+You run a coffee shop and have a system for tracking customer orders.
+Consider the code below and complete the declaration of the second data object so that the
+following code in the main() function runs successfully:*/
+
