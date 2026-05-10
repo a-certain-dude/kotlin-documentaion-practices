2748fcca49138f3150ffd38785f3eca83b303a2d
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-10 07:48:38 +0000

    order interface

--- src/objectDeclaration/CoffeeShop.kt
+++ src/objectDeclaration/CoffeeShop.kt
@@ -6,3 +6,8 @@ You run a coffee shop and have a system for tracking customer orders.
 Consider the code below and complete the declaration of the second data object so that the
 following code in the main() function runs successfully:*/
 
+interface Order {
+    val orderId: String
+    val customerName: String
+    val orderTotal: Double
+}
