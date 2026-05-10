17c33286c70487c981f159922f4f8a60a3f4c3fc
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-10 18:19:26 +0000

    my solution

---
+++ src/objectDeclaration/Temperature.kt
@@ -0,0 +1,39 @@
+package objectDeclaration
+
+/*Exercise 3
+You have an app where you want to record temperatures.
+The class itself stores the information in Celsius, but you want to provide an easy way
+to create an instance in Fahrenheit as well.
+Complete the data class so that the following code in the main()
+function runs successfully:
+Hint
+Use a companion object*/
+
+data class Temperature(val celsius: Double) {
+    val fahrenheit: Double = celsius * 9 / 5 + 32
+
+    companion object Fahrenheit {
+        fun fromFahrenheit(fahrenheit: Double) {
+            val celsius: Double = fahrenheit * 9 / 5 + 32
+            println(celsius)
+        }
+    }
+}
+
+fun main() {
+    (Temperature.fromFahrenheit(3.5))
+    println("^^^^^^^^^^^^")
+
+    Temperature(5.5).apply {
+        println(fahrenheit)
+        Temperature.fromFahrenheit(7.2)
+    }
+
+    println("^^^^^^^^^^^^")
+
+    val fahrenheit = 90.0
+    val temp = Temperature.fromFahrenheit(fahrenheit)
+    println("")
+
+
+}
