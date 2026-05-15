8bd5617bb5529dc90d64e989632714821c1193aa
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-15 17:45:46 +0000

    class rename

--- src/classesAndInterfaces/properties/TemperatureObservable.kt
+++ src/classesAndInterfaces/properties/TemperatureObservable.kt
@@ -2,7 +2,7 @@ package classesAndInterfaces.properties
 
 import kotlin.properties.Delegates.observable
 
-class Temperature {
+class Thermostat {
 
     var temperature: Double by observable(20.4)
     { _, old, new ->
@@ -16,7 +16,7 @@ class Temperature {
 }
 
 fun main() {
-    Temperature().apply {
+    Thermostat().apply {
         temperature = 26.0
         // Warning: Temperature is too high! (20.4`c -> 26.0`c)
         temperature = 24.4
