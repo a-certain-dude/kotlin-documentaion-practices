079c2f63781c86c4dd425012bd19dbc2ad43502a
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-17 17:05:36 +0000

    Travel distance

---
+++ src/classesAndInterfaces/properties/DisplayDistance.kt
@@ -0,0 +1,11 @@
+package classesAndInterfaces.properties
+/*
+
+Exercise 2
+You have a travel app that needs to display distances in both kilometers and miles.
+Create an extension property for the Double type called asMiles to convert a distance
+in kilometers to miles:
+
+The formula to convert kilometers to miles is miles = kilometers * 0.621371 .
+Hint
+Remember that extension properties need a custom get() function.*/
