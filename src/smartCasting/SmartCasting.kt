3c0ca35ab140b500789c677d2b710ed0ec30017b
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-22 13:16:24 +0000

    safe call

--- src/smartCasting/SmartCasting.kt
+++ src/smartCasting/SmartCasting.kt
@@ -1,2 +1,22 @@
 package smartCasting
 
+fun printObjectType(obj: Any) {
+    when (obj) {
+        is Int -> println("It's an Integer type with a value of $obj")
+        !is Double -> println("It's not a Double type, has a value of $obj")
+        else -> println("Unknown type")
+    }
+}
+
+fun main() {
+    printObjectType(obj = 5)
+    printObjectType(obj = mutableListOf(2, 5, 8))
+    printObjectType(obj = "Foo")
+
+    val a: String? = null
+    val b = a as? String
+// Triggers an error at runtime
+    print(b)
+
+
+}
