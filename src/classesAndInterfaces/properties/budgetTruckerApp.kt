271fc1dbec80a21e80a8dab52544082581c4e429
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-22 14:07:20 +0000

    observable

--- src/classesAndInterfaces/properties/budgetTruckerApp.kt
+++ src/classesAndInterfaces/properties/budgetTruckerApp.kt
@@ -13,9 +13,17 @@ the initial budget amount. Within the class, create an observable property calle
 remainingBudget that prints:
 • A warning when the value is lower than 20% of the initial budget.
 • An encouraging message when the budget is increased from the previous value.*/
-
-
-class Budget(totalBudget: Int) {
+class BudgetX(val totalBudget: Int) {
+    var remainingBudget: Int by observable(totalBudget) { _, oldValue, newValue ->
+        if (newValue < totalBudget * 0.2) {
+            println("Warning: Your remaining budget ($newValue) is below 20% of your total budget.")
+        } else if (newValue > oldValue) {
+            println("Good news: Your remaining budget increased to $newValue.")
+        }
+    }
+}
+//
+class Budget(private var totalBudget: Int) {
     var remainingBudget: Int by observable(totalBudget) { _, initVal, new ->
         if (new > 20)
             println("$$initVal Budget has been increased to -> $$new")
