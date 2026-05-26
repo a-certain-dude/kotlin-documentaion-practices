2bfd282e0509f60e7169043e161d207d8c2cbfea
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-26 15:05:58 +0000

    Filter null values

--- src/nullSafety/fillterNullValues.kt
+++ src/nullSafety/fillterNullValues.kt
@@ -1,2 +1,12 @@
 package nullSafety
 
+val emails: List<String?> =
+    mutableListOf(
+        "kwame@gmail.com", "Abena@gmail.com", null,
+        "Abena@gmail.com", null
+    )
+
+val validEmail = emails.filterNotNull()
+fun main() {
+    println(validEmail)
+}
