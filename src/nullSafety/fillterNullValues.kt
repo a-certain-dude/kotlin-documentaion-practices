5f1fa4e0970197600985f15d9926ee796db53cd4
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-26 20:50:40 +0000

    Playing with collections

--- src/nullSafety/fillterNullValues.kt
+++ src/nullSafety/fillterNullValues.kt
@@ -7,6 +7,47 @@ val emails: List<String?> =
     )
 
 val validEmail = emails.filterNotNull()
+
+/*******listOfNotNull*******/
+val serverConfig: Map<String, String?> = mutableMapOf(
+    "appConfig.json" to "App Configuration",
+    "dbConfig.json" to null
+)
+
+val numbersToNumerals: Map<Int, String> = mapOf(
+    1 to "One",
+    2 to "Two",
+    3 to "Three",
+    4 to "Four",
+    5 to "Five",
+    6 to "Six",
+    7 to "Seven",
+    8 to "Eight",
+    9 to "Nine",
+    10 to "Ten",
+)
+
+fun printList(listOfNumbers: MutableList<Int>): MutableList<Int> {
+    listOfNumbers.add(1)
+    listOfNumbers.add(2)
+    listOfNumbers.add(3)
+    listOfNumbers.add(4)
+    listOfNumbers.add(5)
+    listOfNumbers.add(6)
+    listOfNumbers.add(7)
+    listOfNumbers.add(8)
+    listOfNumbers.add(9)
+    listOfNumbers.add(10)
+    listOfNumbers.add(11)
+    return listOfNumbers
+}
+
+const val requestedFile = "appConfig.json"
+val configFiles: List<String> = listOfNotNull(serverConfig["dbConfig.json"])
+
 fun main() {
     println(validEmail)
+    println(configFiles)
+    println(printList(mutableListOf()))
 }
+
