e8147223c9a34ef9a3112618e511897b27fed265
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-28 18:51:12 +0000

    return value or null if absent

--- src/nullSafety/maxOrNullminOrNull.kt
+++ src/nullSafety/maxOrNullminOrNull.kt
@@ -1,2 +1,10 @@
 package nullSafety
 
+val temperature: List<Int> = listOf(4, 56, 34, 23)
+val maxTemperature = temperature.maxOrNull()
+val minTemperature = temperature.minOrNull()
+
+fun main() {
+    println("Maximum temperature is ${maxTemperature ?: "No data"} ")
+    println("Minimum temperature is ${minTemperature ?: "No data"} ")
+}
