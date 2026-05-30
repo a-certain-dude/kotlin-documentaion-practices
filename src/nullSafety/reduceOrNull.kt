ce1d645e004b0724d5408d3cc25771e30d5de055
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-30 21:24:45 +0000

    reduceOrNull usage with empty list

--- src/nullSafety/reduceOrNull.kt
+++ src/nullSafety/reduceOrNull.kt
@@ -9,8 +9,15 @@ https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/reduce-or-null.
 */
 
 fun main() {
-    val pricesOfItems = listOf<Int>(1,7,1,0)
+    val pricesOfItems = listOf<Int>(1, 7, 1, 0)
     val totalPrices = pricesOfItems.reduceOrNull { acc, i -> acc + i }
     println("Calculated items: ${totalPrices ?: "There are no items"} ")
+
+    println("********************************")
+
+    val emptyPricesOfItems = listOf<Int>()
+    val emptyTotalPrices = emptyPricesOfItems.reduceOrNull { acc, i -> acc + i }
+    println("Calculated items: ${emptyTotalPrices ?: "There are no items"} ")
+
 }
 
