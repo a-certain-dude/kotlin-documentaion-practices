82bc2fff1a625ba68a9eef24f64746ff82f94837
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-30 20:51:22 +0000

    comment

--- src/nullSafety/reduceOrNull.kt
+++ src/nullSafety/reduceOrNull.kt
@@ -1,3 +1,11 @@
 package nullSafety
 
-//https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/reduce-or-null.html
+/*
+To use a lambda expression to process each collection item sequentially and create
+an accumulated value (or return a null value if the collection is empty)
+use the reduceOrNull() function
+
+https://kotlinlang.org/api/core/kotlin-stdlib/kotlin.collections/reduce-or-null.html
+*/
+
+
