4d2600917ccb1e449b57472a16943dde1cffc49c
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-30 20:22:39 +0000

    list of user

--- src/nullSafety/firstNotNullOfOrNull.kt
+++ src/nullSafety/firstNotNullOfOrNull.kt
@@ -1,1 +1,12 @@
 package nullSafety
+
+// return the first value that isn't null
+data class User(val name: String?, val age: Int?)
+
+val listOfUsers: List<User> = listOf(
+    User(name = "Mansah", 20),
+    User(name = null, 26),
+    User(name = "Osei", 20),
+    User(name = "Owusuwaa", 84),
+    User(name = "Seiwaa", null),
+)
