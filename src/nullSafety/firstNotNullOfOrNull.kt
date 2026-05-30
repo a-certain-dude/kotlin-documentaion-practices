be1918b1eea8603a5afef64a721f8e9d45d5380a
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-05-30 20:47:08 +0000

    function usages

--- src/nullSafety/firstNotNullOfOrNull.kt
+++ src/nullSafety/firstNotNullOfOrNull.kt
@@ -2,11 +2,23 @@ package nullSafety
 
 // return the first value that isn't null
 data class User(val name: String?, val age: Int?)
+data class AbsentUser(val name: String, val age: Int)
 
 val listOfUsers: List<User> = listOf(
-    User(name = "Mansah", 20),
     User(name = null, 26),
     User(name = "Osei", 20),
     User(name = "Owusuwaa", 84),
+    User(name = "Mansah", 20),
     User(name = "Seiwaa", null),
 )
+
+val absentListOfUsers: List<AbsentUser> = listOf()
+
+fun main() {
+    val firstNonNullName = listOfUsers.firstNotNullOfOrNull { it.age }
+    println(firstNonNullName)
+
+    val firstNonNull = absentListOfUsers.firstOrNull()
+    println(firstNonNull)
+
+}
