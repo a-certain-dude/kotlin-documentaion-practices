e063b41a1695918cfe1eec03155c639b60a67804
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-06-23 19:37:52 +0000

    get notification function

--- src/nullSafety/exercises/notificationSystem.kt
+++ src/nullSafety/exercises/notificationSystem.kt
@@ -6,8 +6,8 @@ You are developing a notification system for an app where users can enable or di
 different types of notifications. Complete the getNotificationPreferences()
 function so that:
 
-1. The validUser variable uses the as? operator to check if user is an instance of the User class.
-If it isn't, return an empty list.
+1. The validUser variable uses the as? operator to check if user is an instance of the
+User class. If it isn't, return an empty list.
 
 2. The userName variable uses the Elvis ?: operator to ensure that the user's name
 defaults to "Guest" if it is null .
@@ -21,11 +21,37 @@ The takeIf() function returns the original value if the given condition is true,
 null . For example:*/
 
 data class User(val name: String?)
+data class Doer(val name: String?)
 
+// How i solved it
+fun getNotificationPreferencesX(user: Any, emailEnabled: Boolean, smsEnabled: Boolean): List<String> {
+    val validUser = user as? User ?: return emptyList()
+    val userName = validUser.name ?: "Guest"
+    return listOfNotNull(userName.takeIf {
+        emailEnabled
+    })
+}
+
+// How i solved it
 fun getNotificationPreferences(user: Any, emailEnabled: Boolean, smsEnabled: Boolean): List<String> {
-    val validUser = user as? User
-    val userName = validUser?.name ?: "Guest"
-    return listOfNotNull(userName)
+    val validUser = user as? User ?: return emptyList()
+    val userName = validUser.name ?: "Guest"
+    return listOfNotNull(
+        "Email notification enabled for $userName".takeIf { emailEnabled },
+        "SMS notification enabled for $userName".takeIf { smsEnabled }
+    )
+}
 
 
+fun main() {
+    val mansahUser = User("Mansah")
+    val userIsNull = User(null)
+    val doer = Doer("Mansah")
+
+    //Here you the user is not an instance of the User class so it prints empty list
+    val notifier = getNotificationPreferences(user = doer, emailEnabled = true, smsEnabled = true)
+    println(notifier)
+
+    println(getNotificationPreferences(mansahUser, emailEnabled = true, smsEnabled = true))
+    println(getNotificationPreferences(userIsNull, emailEnabled = true, smsEnabled = true))
 }
