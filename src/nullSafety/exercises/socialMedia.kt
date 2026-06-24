c181e01a4f4edef0be241934b8990d4bfe8c6017
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-06-24 20:02:40 +0000

    Social Media platform

---
+++ src/nullSafety/exercises/socialMedia.kt
@@ -0,0 +1,9 @@
+package nullSafety.exercises
+
+/*
+Exercise 3
+You are working on a social media platform where users have usernames and account statuses.
+You want to see the list of currently active usernames.
+Complete the getActiveUsernames() function so that the mapNotNull() function has
+a predicate that returns the username if it is active or a null value if it isn't:
+*/
