8d78b7b74fc36ddccef0e12188bfe5f1c86627b9
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-08-10 08:34:30 +0000

    loop@ break@loop

--- src/returnAndjumps/BreakContinue.kt
+++ src/returnAndjumps/BreakContinue.kt
@@ -1,2 +1,15 @@
 package returnAndjumps
+/*
 
+Any expression in Kotlin may be marked with a label.
+Labels have the form of an identifier followed by the @ sign,
+such as abc@ or fooBar@ . To label an expression, just add a label in front of it.
+        */
+
+fun main() {
+    loop@ for (i in 1..20) {
+        for (j in 1..20) {
+            if (j == i) break@loop
+        }
+    }
+}
