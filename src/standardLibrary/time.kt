daa15bc2b04350380d083f384cd3bf2b0b01e81e
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-08-10 08:36:10 +0000

    remove comment

--- src/standardLibrary/time.kt
+++ src/standardLibrary/time.kt
@@ -1,11 +1,27 @@
 package standardLibrary
 
+import java.time.Clock
+import java.time.ZoneId
+import java.util.*
+import kotlin.math.exp
+import kotlin.math.pow
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
 
+//import kotlinx.datetime.*
+
 fun main() {
     val thirtyMinutes: Duration = 30.minutes
     val halfAnHour: Duration = 0.5.hours
-    println(thirtyMinutes==halfAnHour)
+    println(thirtyMinutes == halfAnHour)
+
+    /*   val now = Clock.System.now() // Get current instant
+       println("Current instant: $now")
+       val zone = TimeZone.of("America/New_York")
+       val localDateTime = now.toLocalDateTime(zone)
+       println("Local date-time in NY: $localDateTime")*/
+
+
 }
+
