b50f3b2982aa54f3aa98b33b9c2533a9b2f0497e
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-08-10 09:03:41 +0000

    return to labels

--- src/returnAndjumps/BreakContinue.kt
+++ src/returnAndjumps/BreakContinue.kt
@@ -12,4 +12,13 @@ fun main() {
             if (j == i) break@loop
         }
     }
+    atom()
+}
+
+fun atom() {
+    listOf(1, 2, 3, 4, 5).forEach lit@{
+        if (it == 3) return@lit // local return to the caller - the foreach lambda
+            println(it)
+    }
+   println("done wit explicit label")
 }
