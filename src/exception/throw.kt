d27c4136a04b56e9f622629918329d6444ccf705
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-08-11 10:59:30 +0000

    Exception:throw

--- src/exception/throw.kt
+++ src/exception/throw.kt
@@ -1,1 +1,13 @@
 package exception
+
+val cause = IllegalStateException("Original cause:Illegal state.")
+
+fun main() {
+    var userInput: Int = 0
+
+    if (userInput < 0) {
+        // Throws an IllegalArgumentException if userInput is negative
+        //Additionally, it shows the original cause, represented by the cause IllegalStateException
+        throw IllegalArgumentException("Input must non-negative",cause)
+    }
+}
