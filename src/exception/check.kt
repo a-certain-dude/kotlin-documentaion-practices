48e6673b8bc44834f6faea215ffc504b75a73578
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-08-11 14:08:53 +0000

    Exception:check() try out

--- src/exception/check.kt
+++ src/exception/check.kt
@@ -3,7 +3,7 @@ package exception
 fun main() {
     var someState: String? = null
 
-
+    fun getSomeState() {
 //        checkNotNull(someState)
         check(checkNotNull(someState).isNotEmpty()) {
             "state must be non-empty"
