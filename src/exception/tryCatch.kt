3df29f3a1bda3af1224a520002783737f4b7f360
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-08-12 18:29:28 +0000

    Exception:try catch

--- src/exception/tryCatch.kt
+++ src/exception/tryCatch.kt
@@ -1,2 +1,17 @@
 package exception
 
+fun main() {
+
+    val num = try {
+        count()
+    } catch (_: ArithmeticException) {
+        -1
+    }
+    println(num)
+
+
+}
+fun count(): Int {
+    val a = 0
+    return 10 / a
+}
