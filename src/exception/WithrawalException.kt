1f10258d292298e5008f14c8fc517c23c756442d
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-08-17 18:52:12 +0000

    Exception:multiple catch

--- src/exception/WithrawalException.kt
+++ src/exception/WithrawalException.kt
@@ -1,2 +1,39 @@
 package exception
 
+open class WithdrawalException(message: String) : Exception(message)
+class InsufficientFundsException(message: String) : WithdrawalException(message)
+
+fun processWithdrawal(amount: Double, availableBalance: Double) {
+
+    //When withdrawing at the bank, the amount you enter must either be less or equal to amount left in you bank account
+    if (amount > availableBalance) {
+        throw InsufficientFundsException("Insufficient amount of money")
+    }
+
+    // the double pipe symbol stands for
+    if (amount < 1 || amount % 1 != 0.0) {
+        throw WithdrawalException("Invalid withdrawal amount.")
+    }
+
+    println("Withdrawal Processed.")
+}
+
+fun main() {
+    val availableFunds = 500.0
+    var withdrawalAmount = 500.5
+
+    try {
+        processWithdrawal(amount = withdrawalAmount, availableBalance = availableFunds)
+    }
+    //Using Multiple catch blocks
+    // The order of the catch blocks are very important
+
+    catch (e: InsufficientFundsException) {
+        println("Caught an InsufficientFundsException: ${e.message}")
+    } catch (e: WithdrawalException) {
+        println("Caught a WithdrawalException: ${e.message}")
+    }
+
+
+}
+
