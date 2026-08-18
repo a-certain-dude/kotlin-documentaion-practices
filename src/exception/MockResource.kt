c24e2595b88553654262890024bd9e938d20189d
midnight_bundle <nkwantabisajoseph25@gmail.com>
2026-08-18 18:42:34 +0000

    Exception:finally block MockRes

--- src/exception/MockResource.kt
+++ src/exception/MockResource.kt
@@ -1,2 +1,37 @@
 package exception
 
+
+class MockResource {
+    fun use() {
+        println("Resource being used")
+        val result = 100 / 0
+        println("Result: $result")
+    }
+
+    fun close() {
+        println("Result closed")
+    }
+
+
+}
+
+fun main() {
+
+    val mockRes = MockResource()
+    try {
+        mockRes.use()
+    } finally {
+        mockRes.close()
+    }
+
+    try {
+        MockResource().apply {
+            use()
+        }
+    } finally {
+        MockResource().apply {
+            close()
+        }
+    }
+
+}
